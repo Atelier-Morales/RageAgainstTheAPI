2785af29a1ed97688e090ab3acf86281c4eb0dbe
Atelier-Morales <fmorales@student.42.fr>
2015-12-23 14:05:15 +0100

    final commit

--- getPlace.js
+++ getPlace.js
@@ -1,15 +1,324 @@
 var key = "590d659c-a3f1-49a8-8318-86cd8be744ae";
 var API_URL = "api.navitia.io/v1/coverage/fr-idf";
 var request = require('request');
-var url = "http://" + key + "@" + API_URL + "/pt_objects?q=";
-module.exports = function (origin, cb) {
-    request({
-        url: url + origin + "&type[]=stop_point"
-    }, function (error, response, body) {
-        // Do more stuff with 'body' here
-        if (error)
-            cb(err, null);
-        var result = JSON.parse(body);
-        cb(null, result.pt_objects);
-    });
+var http = require('http');
+var url = "http://" + key + "@" + API_URL;
+
+var lineNumbers = [
+    ["001", "1"],
+    ["002", "2"],
+    ["003", "3"],
+    ["103", "3B"],
+    ["004", "4"],
+    ["005", "5"],
+    ["006", "6"],
+    ["007", "7"],
+    ["107", "7B"],
+    ["008", "8"],
+    ["009", "9"],
+    ["010", "10"],
+    ["011", "11"],
+    ["012", "12"],
+    ["013", "13"],
+    ["014", "14"]
+    ];
+
+exports.getStopPoints = function (cb) {
+    var lines = [];
+    var count = 0;
+    for (var i = 0; i < 16; i++) {
+        (function (i) {
+            http.get(url + "/lines/line:OIF:100110" + lineNumbers[i][0] + ":" + lineNumbers[i][1] + "OIF439/routes?depth=3", function (request) {
+                var result = "";
+                request.setEncoding("utf8");
+                request.on("data", function (data) {
+                    result += data;
+                });
+                request.on("end", function () {
+                    var line = JSON.parse(result);
+                    var stopPoints = [];
+                    count++;
+                    line.routes[0].stop_points.forEach(function (value) {
+                        stopPoints.push({
+                            name: value.label,
+                            coord: value.coord,
+                            connections: []
+                        });
+                    });
+                    lines.push({
+                        name: lineNumbers[i][1],
+                        stations: stopPoints
+                    });
+                    if (count == 16) {
+                        for (var l = 0; l < lines.length; l++) {
+                            for (var j = 0; j < lines[l].stations.length - 1; j++) {
+                                var connect = [];
+                                for (var k = 0; k < lines.length; k++) {
+                                    if (lines[k].name != lines[l].name) {
+                                        for (var m = 0; m < lines[k].stations.length; m++) {
+                                            if (lines[k].stations[m].name === lines[l].stations[j].name)
+                                                connect.push(lines[k].name);
+                                        }
+                                    }
+                                }
+                                lines[l].stations[j].connections = connect;
+                            }
+                        }
+                        cb(null, lines);
+                    }
+                });
+            });
+        })(i);
+    }
+}
+
+function insertCollection(lines, station, callback) {
+    var coll = lines.slice(0);
+    var stationRes = [];
+    (function insertOne() {
+        var record = coll.splice(0, 1)[0];
+        for (var j = 0; j < record.stations.length; j++) {
+            if (record.stations[j].name.toLowerCase().indexOf(station.toLowerCase()) > -1) {
+                var found = false;
+                var l = 0;
+                for (; l < stationRes.length; l++) {
+                    if (stationRes[l].name === record.stations[j].name) {
+                        stationRes[l].line.push(record.name);
+                        found = true;
+                    }
+                }
+                if (found == false) {
+                    stationRes.push({
+                        name: record.stations[j].name,
+                        line: [record.name]
+                    });
+                }
+            }
+        }
+        if (coll.length == 0) {
+            callback(stationRes);
+        } else {
+            setTimeout(insertOne, 0);
+        }
+    })();
+}
+
+var getStationIndex = function (station, line, network) {
+    for (var i = 0; i < network.length; i++) {
+        if (network[i].name === line) {
+            for (var j = 0; j < network[i].stations.length; j++) {
+                if (station == network[i].stations[j].name)
+                    return j;
+            }
+        }
+    }
+    return 0;
+}
+
+var getLineIndex = function (line, network) {
+    for (var i = 0; i < network.length; i++) {
+        if (network[i].name === line) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+var checkDeeper = function (station, lines, network, destination, cb) {
+    var results = [];
+    var found = false;
+
+    for (var i = 0; i < lines.length; i++) {
+        var lineIndex = getLineIndex(lines[i], network);
+        var start = getStationIndex(station, lines[i], network);
+        for (var stationIndex = getStationIndex(station, lines[i], network); stationIndex < network[lineIndex].stations.length; stationIndex++) {
+            if (network[lineIndex].stations[stationIndex].name == destination) {
+                results.push({
+                    line: network[lineIndex].name,
+                    begin: station,
+                    end: network[lineIndex].stations[stationIndex].name,
+                    direction: network[lineIndex].stations[network[lineIndex].stations.length - 1].name,
+                    stations: stationIndex - start
+                });
+            }
+        }
+        for (var stationIndex = getStationIndex(station, lines[i], network); stationIndex >= 0; stationIndex--) {
+            if (network[lineIndex].stations[stationIndex].name == destination) {
+                results.push({
+                    line: network[lineIndex].name,
+                    begin: station,
+                    end: network[lineIndex].stations[stationIndex].name,
+                    direction: network[lineIndex].stations[0].name,
+                    stations: start - stationIndex
+                });
+            }
+        }
+    }
+    if (results.length == 0)
+        cb(false);
+    else
+        cb(results);
+}
+
+var checkStationInLine = function (station, lines, network, destination, cb) {
+    var results = [];
+    var found = false;
+
+    for (var i = 0; i < lines.length; i++) {
+        var lineIndex = getLineIndex(lines[i], network);
+        var start = getStationIndex(station, lines[i], network);
+        for (var stationIndex = getStationIndex(station, lines[i], network); stationIndex < network[lineIndex].stations.length; stationIndex++) {
+            if (network[lineIndex].stations[stationIndex].name == destination) {
+                if (stationIndex - start != 0) {
+                    results.push([{
+                        line: network[lineIndex].name,
+                        begin: station,
+                        end: network[lineIndex].stations[stationIndex].name,
+                        direction: network[lineIndex].stations[network[lineIndex].stations.length - 1].name,
+                        stations: stationIndex - start
+                    }]);
+                }
+            } else if (network[lineIndex].stations[stationIndex].name != destination && network[lineIndex].stations[stationIndex].connections.length > 0) {
+                checkDeeper(network[lineIndex].stations[stationIndex].name, network[lineIndex].stations[stationIndex].connections, network, destination, function (data) {
+                    if (data != false) {
+                        if (stationIndex - start != 0) {
+                            results.push([{
+                                line: network[lineIndex].name,
+                                begin: station,
+                                end: network[lineIndex].stations[stationIndex].name,
+                                direction: network[lineIndex].stations[network[lineIndex].stations.length - 1].name,
+                                stations: stationIndex - start
+                            }, data[0]]);
+                        }
+                    }
+                });
+            }
+        }
+        for (var stationIndex = getStationIndex(station, lines[i], network); stationIndex >= 0; stationIndex--) {
+            if (network[lineIndex].stations[stationIndex].name == destination) {
+                if (start - stationIndex != 0) {
+                    results.push([{
+                        line: network[lineIndex].name,
+                        begin: station,
+                        end: network[lineIndex].stations[stationIndex].name,
+                        direction: network[lineIndex].stations[0].name,
+                        stations: start - stationIndex
+                    }]);
+                }
+            } else if (network[lineIndex].stations[stationIndex].name != destination && network[lineIndex].stations[stationIndex].connections.length > 0) {
+                checkDeeper(network[lineIndex].stations[stationIndex].name, network[lineIndex].stations[stationIndex].connections, network, destination, function (data) {
+                    if (data != false) {
+                        if (start - stationIndex != 0) {
+                            results.push([{
+                                line: network[lineIndex].name,
+                                begin: station,
+                                end: network[lineIndex].stations[stationIndex].name,
+                                direction: network[lineIndex].stations[0].name,
+                                stations: start - stationIndex
+                            }, data[0]]);
+                        }
+                    }
+                });
+            }
+        }
+    }
+    if (results.length == 0)
+        cb(false);
+    else
+        cb(results);
+}
+
+exports.processJourney = function (origin, destination, network, cb) {
+    var line = origin.line.slice(0);
+    var journey = [];
+    var journeyCount = 0;
+    (function insertJourney() {
+        var record = line.splice(0, 1)[0];
+        var lineIndex = getLineIndex(record, network);
+        var start = getStationIndex(origin.name, record, network);
+        var directLine = false;
+        for (var stationIndex = getStationIndex(origin.name, record, network); stationIndex < network[lineIndex].stations.length; stationIndex++) {
+            if (network[lineIndex].stations[stationIndex].name == destination.name) {
+                journey.push([{
+                    line: network[lineIndex].name,
+                    begin: origin.name,
+                    end: network[lineIndex].stations[stationIndex].name,
+                    direction: network[lineIndex].stations[network[lineIndex].stations.length - 1].name,
+                    stations: stationIndex - start
+                    }]);
+                journeyCount++;
+                directLine = true;
+            }
+        }
+        for (var stationIndex = getStationIndex(origin.name, record, network); stationIndex >= 0; stationIndex--) {
+            if (network[lineIndex].stations[stationIndex].name == destination.name) {
+                journey.push([{
+                    line: network[lineIndex].name,
+                    begin: origin.name,
+                    end: network[lineIndex].stations[stationIndex].name,
+                    direction: network[lineIndex].stations[0].name,
+                    stations: start - stationIndex
+                    }]);
+                journeyCount++;
+                directLine = true;
+            }
+        }
+        if (directLine == false) {
+            for (var stationIndex = getStationIndex(origin.name, record, network); stationIndex < network[lineIndex].stations.length; stationIndex++) {
+                if (network[lineIndex].stations[stationIndex].connections.length > 0) {
+                    checkStationInLine(network[lineIndex].stations[stationIndex].name, network[lineIndex].stations[stationIndex].connections, network, destination.name, function (data) {
+                        if (data != false) {
+                            for (n = 0; n < data.length; n++) {
+                                var total = [];
+                                if (stationIndex - start != 0) {
+                                    total.push({
+                                        line: network[lineIndex].name,
+                                        begin: origin.name,
+                                        end: network[lineIndex].stations[stationIndex].name,
+                                        direction: network[lineIndex].stations[network[lineIndex].stations.length - 1].name,
+                                        stations: stationIndex - start
+                                    });
+                                }
+                                for (var q = 0; q < data[n].length; q++) {
+                                    total.push(data[n][q]);
+                                }
+                                journey.push(total);
+                                journeyCount++;
+                            }
+                        }
+                    });
+                }
+            }
+            for (var stationIndex = getStationIndex(origin.name, record, network); stationIndex >= 0; stationIndex--) {
+                if (network[lineIndex].stations[stationIndex].connections.length > 0) {
+                    checkStationInLine(network[lineIndex].stations[stationIndex].name, network[lineIndex].stations[stationIndex].connections, network, destination.name, function (data) {
+                        if (data != false) {
+                            for (n = 0; n < data.length; n++) {
+                                var total = [];
+                                if (start - stationIndex != 0) {
+                                    total.push({
+                                        line: network[lineIndex].name,
+                                        begin: origin.name,
+                                        end: network[lineIndex].stations[stationIndex].name,
+                                        direction: network[lineIndex].stations[0].name,
+                                        stations: start - stationIndex
+                                    });
+                                }
+                                for (var q = 0; q < data[n].length; q++) {
+                                    total.push(data[n][q]);
+                                }
+                                journey.push(total);
+                                journeyCount++;
+                            }
+                        }
+                    });
+                }
+            }
+        }
+        if (journeyCount > 5 || line.length == 0) {
+            cb(journey);
+        } else {
+            setTimeout(insertJourney, 0);
+        }
+    })();
 }
--- ratp.js
+++ ratp.js
@@ -1,22 +1,188 @@
 var getPlace = require('./getPlace');
 var origin = process.argv[2];
-//var arrival = process.argv[3];
+var destination = process.argv[3];
 
-getPlace(origin, function (err, data) {
-    if (err)
-        console.log(err);
-    else {
-        console.log(data[0]);
-        process.stdin.setEncoding('utf8');
-        process.stdin.on('readable', function () {
-            var chunk = process.stdin.read();
-            if (chunk !== null) {
-                process.stdout.write('data: ' + chunk);
-            }
-        });
-
-        process.stdin.on('end', function () {
-            process.stdout.write('end');
-        });
+function setTerminus(terminus, station) {
+    var line1 = [
+        "Brochant (Paris)",
+        "Porte de Clichy (Paris)",
+        "Mairie de Clichy (Clichy)"
+    ];
+    var line2 = [
+        "Pierre et Marie Curie (Ivry-sur-Seine)",
+        "Porte d'Ivry (Paris)",
+        "Porte de Choisy (Paris)",
+        "Porte d'Italie (Paris)"
+        ];
+    if (line1.indexOf(station) > - 1) {
+        return "Asnieres Gennevilliers Les Courtilles (Gennevilliers) (Asnières-sur-Seine)";
+    } else if (line2.indexOf(station) > - 1) {
+        return "Mairie d'Ivry (Ivry-sur-Seine)";
     }
-});
+    return terminus;
+}
+
+function insertCollection(lines, station, callback) {
+    var coll = lines.slice(0);
+    var stationRes = [];
+    (function insertOne() {
+        var record = coll.splice(0, 1)[0];
+        for (var j = 0; j < record.stations.length; j++) {
+            if (record.stations[j].name.toLowerCase().indexOf(station.toLowerCase()) > -1) {
+                var found = false;
+                var l = 0;
+                for (; l < stationRes.length; l++) {
+                    if (stationRes[l].name === record.stations[j].name) {
+                        stationRes[l].line.push(record.name);
+                        found = true;
+                    }
+                }
+                if (found == false) {
+                    stationRes.push({
+                        name: record.stations[j].name,
+                        line: [record.name]
+                    });
+                }
+            }
+        }
+        if (coll.length == 0) {
+            callback(stationRes);
+        } else {
+            setTimeout(insertOne, 0);
+        }
+    })();
+}
+
+var main = function () {
+    if (origin == undefined || destination == undefined) {
+        console.log("Please choose a departure and a destination for your journey.");
+        process.exit();
+    }
+    getPlace.getStopPoints(function (err, data) {
+        if (err) {
+            console.log(err);
+            process.exit();
+        } else {
+            var journey = [];
+            var lines = data;
+
+            insertCollection(lines, origin, function (data) {
+                if (data.length == 0) {
+                    console.log("wrong input");
+                    process.exit();
+                }
+                var station1 = data;
+                insertCollection(lines, destination, function (data) {
+                    if (data.length == 0) {
+                        console.log("wrong input");
+                        process.exit();
+                    }
+                    var station2 = data;
+                    var log1 = "";
+                    var log2 = "";
+                    for (var i = 0; i < station1.length; i++)
+                        log1 += i + " - " + station1[i].name + " (line " + station1[i].line + ")  ";
+                    for (var i = 0; i < station2.length; i++)
+                        log2 += i + " - " + station2[i].name + " (line " + station2[i].line + ")  ";
+                    process.stdin.setEncoding('utf8');
+                    process.stdin.setMaxListeners(0);
+                    process.stdin.on('readable', function () {
+                        var chunk = process.stdin.read();
+
+                        if (chunk !== null) {
+                            if (isNaN(chunk) || Number(chunk) < 0)
+                                console.log('wrong input');
+                            else {
+                                if (journey.length == 0 && Number(chunk) <= station1.length - 1) {
+                                    journey.push(station1[Number(chunk)]);
+                                    console.log(log2);
+                                    console.log("Choose the station ID:");
+                                } else if (journey.length > 0 && Number(chunk) <= station2.length - 1) {
+                                    journey.push(station2[Number(chunk)]);
+                                    getPlace.processJourney(journey[0], journey[1], lines, function (data) {
+                                        var shortest_journey = 0;
+                                        var set = 0;
+                                        for (var index = 0; index < data.length; index++) {
+                                            if (shortest_journey == 0) {
+                                                for (p = 0; p < data[index].length; p++) {
+                                                    if (p > 0)
+                                                        shortest_journey += 5;
+                                                    shortest_journey += data[index][p].stations * 2;
+                                                }
+                                            }
+                                            else {
+                                                var buffer = 0;
+                                                for (y = 0; y < data[index].length; y++) {
+                                                    if (y > 0)
+                                                        buffer += 5;
+                                                    buffer += data[index][y].stations * 2;
+                                                }
+                                                if (buffer < shortest_journey) {
+                                                    shortest_journey = buffer;
+                                                    set = index;
+                                                }
+                                            }
+                                        }
+                                        var date = new Date();
+                                        var hour = date.getHours();
+                                        var min  = date.getMinutes();
+                                        var mins = 0;
+                                        if (min < 10)
+                                            mins = "0"+min;
+                                        else
+                                            mins = min;
+                                        console.log("for requested departure time: "+hour+":"+mins);
+                                        var c = 0;
+                                        var totalStations = 0;
+                                        for (; c < data[set].length; c++) {
+                                            min += 2;
+                                            if (min >= 60) {
+                                                hour += 1;
+                                                if (hour > 24)
+                                                    hour = 00;
+                                                min = min - 60;
+
+                                            }
+                                            if (min < 10)
+                                                mins = "0"+min;
+                                            else
+                                                mins = min;
+                                            var terminus = setTerminus(data[set][c].direction, data[set][c].end);
+                                            console.log("Line "+data[set][c].line+" from "+data[set][c].begin+" to "+data[set][c].end+" direction: "+terminus+" at "+hour+":"+mins+" total stations: "+data[set][c].stations);
+                                            if (c > 0)
+                                                min += 5;
+                                            min += data[set][c].stations * 2;
+                                            if (min > 60) {
+                                                hour += 1;
+                                                if (hour > 24)
+                                                    hour = 00;
+                                                min = min - 60;
+
+                                            }
+                                            totalStations += data[set][c].stations;
+                                        }
+                                        if (min < 10)
+                                            min = "0"+min;
+                                        console.log("Arrival at: "+hour+":"+min+" Total stations: "+totalStations+", transfers: "+ (c - 1));
+                                        process.stdin.emit("end");
+                                    });
+                                } else
+                                    console.log("wrong input");
+                            }
+                        } else {
+                            console.log(log1);
+                            console.log("Choose the station ID:");
+                        }
+                    });
+
+                    process.stdin.on('end', function () {
+
+                        process.exit();
+                    });
+                });
+            });
+        }
+    });
+}
+
+main();
